c4e3a16789211a9ff4233c435a6778ab50e6cd33
MystryDawid <37050571+MystryDawid@users.noreply.github.com>
2020-03-15 12:02:36 +0100

    weighted astar

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# aStar
-While programing a star path finder algorithm i have learned python OOP
+# Weighted a star
+While programing weighted a star path finder algorithm i have learned python OOP
 
 
-This aStar assumes size and difficulty (from 0 to 100)
-Return the path (position x and y) if the path was found or false, if not, the maze and each node that was in the open set.
+This aStar take as input size, difficulty (from 0 to 100), e (from 0 to 100) if e is small algorithm is taking longer but it return better path in the opposite case algorithm is fast but path isn't the best. 
+
+Return the path (position x and y of every node of path) or false if the path was not found, maze, each node that was in the open set and how long algorithm was working.
--- aStar.py
+++ aStar.py
@@ -1,9 +1,10 @@
 from random import random
-import numpy as np
+import math
+import time as _time
 
 
 class Node:
-    def __init__(self, parent=None, position=None):     # initialization of objects
+    def __init__(self, parent=None, position=None):     # constructor of objects
         self.parent = parent
         self.position = position
 
@@ -11,73 +12,84 @@ class Node:
         self.h = 0
         self.f = 0
 
-    def __eq__(self, other):                    # function for comparing objects
+    def __eq__(self, other):    # function for comparing objects
         return self.position == other.position
 
-    def __hash__(self):                         # function for hashing objects
+    def __hash__(self):         # function for hashing objects
         return hash(self.position)
 
-    def __sub__(self, other):  # function for subtractions objects
-        return np.subtract(self.position, other.position)
+    def __sub__(self, other):   # function for subtractions objects
+        return self.position - other.position
 
 
-def astar(size, difficulty):
+def astar(size, difficulty, e):
+    start_time = _time.time()   # starting timer
     grid = size
     diff = difficulty / 100
+    e = e/100
+    n = grid * 2.5
 
-    maze = [[1 if random() < diff else 0 for x in range(grid)] for y in range(grid)]
     # creating a maze size x size
+    maze = [[1 if random() < diff else 0 for x in range(grid)] for y in range(grid)]
 
+    # making sure that start and goal are not wall
     maze[0][0] = 0
-    maze[grid - 1][grid - 1] = 0    # making sure that start and goal are not wall
+    maze[grid - 1][grid - 1] = 0
 
+    # setting start and end node
     start_node = Node(None, (0, 0))
-    end = Node(None, (grid - 1, grid - 1))  # setting start and end node
+    end = Node(None, (grid - 1, grid - 1))
 
     open_set = []
     closed_set = []
     o = []
 
-    open_set.append(start_node)
-    o.append(o.append([int(start_node.position[0]), int(start_node.position[1])]))
     # adding start node to open set and o
-    while open_set:
+    open_set.append(start_node)
+    o.append(start_node.position)
 
+    while open_set:
         current_node = open_set[0]
         current_index = 0
 
-        for index, item in enumerate(open_set):     # looking in open set for the closest not to the end
+        for index, item in enumerate(open_set):     # looking in open set for the closest node to the end
             if item.f < current_node.f:
                 current_node = item
                 current_index = index
 
-        open_set.pop(current_index)                 # deleting closest node from open set
-        closed_set.append(current_node)             # and adding it to the close set
+        if current_node == end:     # if path is founded
 
-        if current_node == end:                     # if path is founded
-            path = []
-            while current_node.parent:                  # from latest node to the node after start
-                path.append([(int(current_node.position[0]), int(current_node.position[1]))])
-                # adding nod position to the path table
-                current_node = current_node.parent      # changing node to previous one
-            path.append([(int(current_node.position[0]), int(current_node.position[1]))])
-            # adding the start node position
-            return [path[::-1], maze, o]                # returning revers path,
-            # maze and position of every node that was in open set
+            path = [current_node.position]      # adding the start node position
 
-        for new_position in [(-1, -1), (-1, 1), (1, -1), (1, 1), (0, -1), (0, 1), (-1, 0), (1, 0)]:
+            while current_node.parent:
+                current_node = current_node.parent      # adding node position to the path
+                path.append(current_node.position)      # changing node to previous one
 
-            position = np.add(current_node.position, new_position)
+            time = _time.time() - start_time    # stopping timer
+            # returning revers path, maze and position of every node that was in open set and time
+            return path[::-1], maze, o, time
+
+        open_set.pop(current_index)         # deleting closest node from open set
+        closed_set.append(current_node)     # and adding it to the close set
+
+        d = closed_set[-1].g    # calculating d (depth of search)
+        w = (0.4 + e - e * (d / n))     # calculating w (dynamic weight)
+
+        # looking in neighbors for new nodes
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+
             # creating a new position close to current node position
+            position = current_node.position[0] + new_position[0], current_node.position[1] + new_position[1]
 
+            # checking if position is walkable if not we skip one loop
             if position[0] > (len(maze) - 1) or\
                     position[0] < 0 or\
                     position[1] > (len(maze[len(maze) - 1]) - 1) or\
                     position[1] < 0 or\
-                    maze[position[0]][position[1]] != 0:    # checking if position is not walkable,
-                continue                                    # if if not we skip one loop
+                    maze[position[0]][position[1]] != 0:
+                continue
 
-            new_node = Node(current_node, position)     # if so creating a new node
+            new_node = Node(current_node, position)  # creating a new node
 
             if new_node in closed_set:  # checking if node is in closet_set, if is we skip one loop
                 continue
@@ -85,20 +97,19 @@ def astar(size, difficulty):
             if new_node in open_set:    # checking if node is in open_set, if is we skip one loop
                 continue
 
-            new_node.g = current_node.g + int(np.linalg.norm(new_node.position - current_node.position))
-            # calculating the g
+            # calculating g
+            new_node.g = current_node.g + math.sqrt((new_node.position[0] - current_node.position[0])**2
+                                                    + (new_node.position[1] - current_node.position[1])**2)
+            # calculating h
+            new_node.h = math.sqrt((new_node.position[0] - end.position[0])**2
+                                   + (new_node.position[1] - end.position[1])**2)
+            # calculating f using dynamic weight
+            new_node.f = new_node.g + w*new_node.h
 
-            new_node.h = np.linalg.norm(new_node.position - end.position)
-            # calculating the h
+            open_set.append(new_node)       # adding new node to open_set
+            o.append(new_node.position)     # adding new nodes position to o
 
-            new_node.f = new_node.g + new_node.h    # calculating the f
-
-            open_set.append(new_node)    # adding new node to open_set
-            o.append([(int(new_node.position[0]), int(new_node.position[1]))])
-            # adding new nodes position to o
-
-    return False, maze, o   # if end wasn't found we return false, maze and o
-
-
+    time = _time.time() - start_time    # stopping timer
+    return False, maze, o, time  # if end wasn't found we return false, maze, o and time
 
 
